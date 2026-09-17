014ceadb10aac67a473133018b0931aec6c4a3b8
Raymond Coetzee <coetzeer@gmail.com>
2026-09-17 07:00:43 +0200

    Update hermes-webui to exp-v0.52.315

--- Formula/hermes-webui.rb
+++ Formula/hermes-webui.rb
@@ -1,8 +1,8 @@
 class HermesWebui < Formula
   desc "Lightweight, dark-themed web interface for Hermes Agent"
   homepage "https://github.com/nesquena/hermes-webui"
-  url "https://github.com/nesquena/hermes-webui/archive/refs/tags/exp-v0.52.310.tar.gz"
-  sha256 "f20e021fea68076701a6af86f8e3771662baafed2b6074929ffa2b63853a4632"
+  url "https://github.com/nesquena/hermes-webui/archive/refs/tags/exp-v0.52.315.tar.gz"
+  sha256 "d5be796e8f8942bd6509eae024b59d5d837e9e50f7db2e8a2c882a259c8cb2b8"
   license "MIT"
   head "https://github.com/nesquena/hermes-webui.git", branch: "master"
 
